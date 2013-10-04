d6a7fb06c6fc61a41db30b4f3226683e97e148d6
Nathan Kluth <nlkluth@gmail.com>
2013-10-04 15:52:38 -0600

    working on player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour 
+{
+	public float moveSpeed = 0.1f;
+	public float maxSpeed = 10f;
+	public float speedSmooth = 0.4f;
+	
+	void FixedUpdate()
+	{
+		float movement = Input.GetAxis("Horizontal") * moveSpeed;
+		if (movement > 0) {
+			float speed = Mathf.Lerp(movement, maxSpeed, speedSmooth * Time.deltaTime);
+        	transform.Translate(0f, 0f, speed);
+			
+			Debug.Log(speed);
+		}
+	}
+}
