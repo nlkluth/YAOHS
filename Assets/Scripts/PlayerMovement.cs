f651c27a42f8bf245e42705ac8ad5e16f521c4af
Nathan Kluth <nlkluth@gmail.com>
2013-10-04 22:03:17 -0600

    cleaned up code a bit

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,23 +7,18 @@ public class PlayerMovement : MonoBehaviour
 	public float maxSpeed = 10f;
 	
 	void FixedUpdate()
-	{
+	{	
 		if (Input.GetButton("Horizontal"))
-		{
 			MoveCharacter();
-		}
 	}
 	
 	void MoveCharacter()
 	{
 		moveSpeed += moveSpeed * Time.deltaTime;
+		
 		if (moveSpeed < maxSpeed)
-		{
 			transform.Translate(0f, 0f, moveSpeed);
-		}
 		else
-		{
 			transform.Translate(0f, 0f, maxSpeed);
-		}
 	}
 }
