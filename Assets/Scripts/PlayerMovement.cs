adc999fbc5c9235ebb454eef6f0138aa9e8a1e15
Nathan Kluth <nlkluth@gmail.com>
2013-10-05 19:05:35 -0600

    Decelerate instead of dead-stop when letting go of button

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,25 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour 
 {
 	public float moveSpeed = 0.1f;
+	public float minSpeed = 0.2f;
 	public float maxSpeed = 0.5f;
+	public float damper = 0.6f;
+	
+	private bool decelerating = false;
 	
 	void FixedUpdate()
 	{	
 		if (Input.GetButton("Horizontal"))
+		{
+			decelerating = false;
 			MoveCharacter();
+		}
 		
 		if (Input.GetButtonUp("Horizontal"))
-			moveSpeed = 0.1f;
+			decelerating = true;
+		
+		if (decelerating)
+			SlowCharacterDown();
 	}
 	
 	void MoveCharacter()
@@ -23,5 +33,14 @@ public class PlayerMovement : MonoBehaviour
 			transform.Translate(0f, 0f, moveSpeed);
 		else
 			transform.Translate(0f, 0f, maxSpeed);
+	}
+	
+	void SlowCharacterDown()
+	{
+		moveSpeed -= moveSpeed * (Time.deltaTime * damper);
+		transform.Translate(0f, 0f, moveSpeed);
+		
+		if (moveSpeed < minSpeed)
+			decelerating = false;
 	}
 }
