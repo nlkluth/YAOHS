ce24add676166e6b43e877d3c577e9756d222044
Nathan Kluth <nlkluth@gmail.com>
2014-03-14 11:53:43 -0600

    attack on keypress

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAttack : MonoBehaviour 
+{
+	public GameObject target;
+
+	void Start() 
+	{
+		
+	}
+	
+	void Update() 
+	{
+		if (Input.GetKeyUp(KeyCode.F))
+		{
+			Attack();
+		}
+	}
+
+	private void Attack()
+	{
+		EnemyHealth enemyHealth = (EnemyHealth)target.GetComponent("EnemyHealth");
+		enemyHealth.AdjustCurrentHealth(-10);
+	}
+}
