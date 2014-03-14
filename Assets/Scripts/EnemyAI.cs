f6b5fbcca5f0ac4969dca00e8993328c2e06308c
Nathan Kluth <nlkluth@gmail.com>
2014-03-14 12:17:27 -0600

    enemy stops moving if in melee range

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -6,6 +6,7 @@ public class EnemyAI : MonoBehaviour
 	public Transform target;
 	public float moveSpeed;
 	public float rotationSpeed;
+	public float maxDistance;
 
 	private Transform localTransform;
 
@@ -18,11 +19,16 @@ public class EnemyAI : MonoBehaviour
 	{
 		GameObject player = GameObject.FindGameObjectWithTag("Player");
 		target = player.transform;
+		maxDistance = 2f;
 	}
 	
 	void Update () 
 	{
 		localTransform.rotation = Quaternion.Slerp(localTransform.rotation, Quaternion.LookRotation(target.position - localTransform.position), rotationSpeed * Time.deltaTime);
-		localTransform.position += localTransform.forward * moveSpeed * Time.deltaTime;
+
+		if (Vector3.Distance(target.position, localTransform.position) > maxDistance)
+		{
+			localTransform.position += localTransform.forward * moveSpeed * Time.deltaTime;
+		}
 	}
 }
