da11a25ee8551613323f80f963968c14245e84e3
Nathan Kluth <nlkluth@gmail.com>
2014-03-14 09:27:05 -0600

    avoid recalculation - test

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -5,9 +5,26 @@ public class PlayerHealth : MonoBehaviour
 {
 	public int maxHealth = 100;
 	public int currentHealth = 100;
+	public float healthBarLength;
+
+	void Start()
+	{
+		healthBarLength = Screen.width / 2;
+	}
+
+	void Update()
+	{
+		AdjustCurrentHealth(0);
+	}
+
+	public void AdjustCurrentHealth(int adjustment)
+	{
+		currentHealth += adjustment;
+		healthBarLength = (Screen.width/2) * (currentHealth / (float)maxHealth);
+	}
 
 	private void OnGUI()
 	{
-		GUI.Box(new Rect(10, 10, (Screen.width/2)/(maxHealth/currentHealth), 20), currentHealth + " / " + maxHealth);
+		GUI.Box(new Rect(10, 10, healthBarLength, 20), currentHealth + " / " + maxHealth);
 	}
 }
