e13c03ec431d91f7b01f4f8a0b9a2bdb7545b032
Nathan Kluth <nlkluth@gmail.com>
2014-03-14 14:18:40 -0600

    list all enemies and target first one on tab

---
+++ Assets/Scripts/Targetting.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class Targetting : MonoBehaviour 
+{
+	public List<Transform> targets;
+
+	public Transform selectedTarget;
+
+	void Start() 
+	{
+		targets = new List<Transform>();
+		selectedTarget = null;
+
+		AddAllTargets();
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Tab))
+	    {
+			TargetEnemy();
+		}
+	}
+
+	public void AddAllTargets()
+	{
+		GameObject[] targets = GameObject.FindGameObjectsWithTag("Enemy");
+
+		foreach (GameObject target in targets)
+		{
+			AddTarget(target.transform);
+		}
+	}
+
+	public void AddTarget(Transform target)
+	{
+		targets.Add(target);
+	}
+
+	private void TargetEnemy()
+	{
+		selectedTarget = targets[0];
+	}
+}
