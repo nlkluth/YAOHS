a996e467957f9f091de72b29994edf15fd9d0c7c
Nathan Kluth <nlkluth@gmail.com>
2014-03-14 09:32:47 -0600

    quick enemy health

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHealth : MonoBehaviour 
+{
+	public int maxHealth = 100;
+	public int currentHealth = 100;
+	public float healthBarLength;
+
+	void Start()
+	{
+		healthBarLength = Screen.width / 2;
+	}
+
+	void Update()
+	{
+		AdjustCurrentHealth(0);
+	}
+
+	public void AdjustCurrentHealth(int adjustment)
+	{
+		currentHealth += adjustment;
+
+		if (currentHealth < 0) 
+		{
+			currentHealth = 0;
+		}
+
+		if (currentHealth > maxHealth)
+		{
+			currentHealth = maxHealth;
+		}
+
+		if (maxHealth < 1)
+		{
+			maxHealth = 1;
+		}
+
+		healthBarLength = (Screen.width/2) * (currentHealth / (float)maxHealth);
+	}
+
+	private void OnGUI()
+	{
+		GUI.Box(new Rect(10, 40, healthBarLength, 20), currentHealth + " / " + maxHealth);
+	}
+}
