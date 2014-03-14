8a6055d3d7d48fbb56a45ad5424aab9561ae8e22
Nathan Kluth <nlkluth@gmail.com>
2014-03-14 09:20:46 -0600

    stubbing player health

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour 
+{
+	public int maxHealth = 100;
+	public int currentHealth = 100;
+
+	private void OnGUI()
+	{
+		GUI.Box(new Rect(10, 10, (Screen.width/2)/(maxHealth/currentHealth), 20), currentHealth + " / " + maxHealth);
+	}
+}
