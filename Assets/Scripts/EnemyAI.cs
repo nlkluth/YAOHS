faafa632ffb787d6c8ca72135e5e465c4c96d805
Nathan Kluth <nlkluth@gmail.com>
2014-03-14 09:38:43 -0600

    setting basic enemy ai

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour 
+{
+	public Transform target;
+	public int moveSpeed;
+
+	private Transform localTransform;
+
+	void Awake()
+	{
+		localTransform = transform;
+	}
+
+	void Start () 
+	{
+		GameObject gameObject = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	void Update () 
+	{
+	
+	}
+}
