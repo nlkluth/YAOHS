0ac11fb5e0b6b70ffa5b0b508a8b4d2ad2aebb2e
Nathan Kluth <nlkluth@gmail.com>
2014-03-23 13:25:22 -0600

    skill class

---
+++ Assets/Scripts/Character Classes/Skill.cs
@@ -0,0 +1,21 @@
+﻿public class Skill : BaseStat 
+{
+	public Skill()
+	{
+		Known = false;
+		ExpToLevel = 25;
+		LevelModifier = 1.1f;
+	}
+
+	public bool Known { get; set; }
+}
+
+public enum SkillName
+{
+	Melee_Offense,
+	Melee_Defense,
+	Ranged_Offense,
+	Ranged_Defense,
+	Magic_Offense,
+	Magic_Defense
+}
