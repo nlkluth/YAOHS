155c63b70996c747e208510d149b94f11150f3f8
Nathan Kluth <nlkluth@gmail.com>
2014-03-23 12:55:35 -0600

    modified stats

---
+++ Assets/Scripts/Character Classes/ModifiedStat.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+public class ModifiedStat : BaseStat 
+{
+	private List<ModifyingAttribute> _modifiers;
+	private int _modifiedValue;
+
+	public ModifiedStat()
+	{
+		_modifiers = new List<ModifyingAttribute>();
+		_modifiedValue = 0;
+	}
+
+	public void AddModifier(ModifyingAttribute modifier)
+	{
+		_modifiers.Add(modifier);
+	}
+
+	private void CalculateModifiedValue()
+	{
+		_modifiedValue = 0;
+
+		if (_modifiers.Count > 0)
+		{
+			foreach (ModifyingAttribute attribute in _modifiers)
+			{
+				_modifiedValue += (int)(attribute.attribute.AdjustedBaseValue * attribute.ratio);
+			}
+		}
+	}
+
+	public new int AdjustedBaseValue
+	{
+		get { return BaseValue + BuffValue + _modifiedValue; }
+	}
+
+	public void Update()
+	{
+		CalculateModifiedValue();
+	}
+}
+
+public struct ModifyingAttribute
+{
+	public Attribute attribute;
+	public float ratio;
+
+}
