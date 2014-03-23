8532a3d0ca065d9fef7ace3d8aa97a3a98fe312e
Nathan Kluth <nlkluth@gmail.com>
2014-03-22 22:29:46 -0600

    added attribute child of basestat

---
+++ Assets/Scripts/Character Classes/Attribute.cs
@@ -0,0 +1,20 @@
+﻿
+public class Attribute : BaseStat
+{
+	public Attribute()
+	{
+		ExpToLevel = 50;
+		LevelModifier = 1.05f;
+	}
+
+	public enum AttributeName
+	{
+		Might,
+		Consitution,
+		Nimbleness,
+		Speed,
+		Concentration,
+		Willpower,
+		Charisma
+	}
+}
