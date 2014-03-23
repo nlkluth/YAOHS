05d68be93ee411e98e8d727b9e4d95e66d556cd5
Nathan Kluth <nlkluth@gmail.com>
2014-03-23 13:20:29 -0600

    adding vitals

---
+++ Assets/Scripts/Character Classes/Vital.cs
@@ -0,0 +1,31 @@
+﻿public class Vital : ModifiedStat 
+{
+	private int _currentValue;
+
+	public Vital()
+	{
+		_currentValue = 0;
+		ExpToLevel = 50;
+		LevelModifier = 1.1f;
+	}
+
+	private int Currentvalue
+	{ 
+		get
+		{
+			if (_currentValue > AdjustedBaseValue)
+			{
+				_currentValue = AdjustedBaseValue;
+			}
+			return _currentValue;
+		}
+		set; 
+	}
+}
+
+public enum VitalName
+{
+	Health,
+	Energy,
+	Mana
+}
