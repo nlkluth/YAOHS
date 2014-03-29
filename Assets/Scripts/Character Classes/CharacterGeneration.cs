70bd0389989a7dcf455ace405f5983585ee973b7
Nathan Kluth <nlkluth@gmail.com>
2014-03-29 17:35:21 -0600

    bit of method extracting

--- Assets/Scripts/Character Classes/CharacterGeneration.cs
+++ Assets/Scripts/Character Classes/CharacterGeneration.cs
@@ -14,13 +14,34 @@ public class CharacterGeneration : MonoBehaviour
 
 	void OnGUI()
 	{
+		DisplayName();
+		DisplayAttributes();
+		DisplayAttributes();
+		DisplaySkills();
+	}
+
+	private void DisplayName()
+	{
 		GUI.Label(new Rect(10, 10, 50, 25), "Name: ");
 		_toon.Name = GUI.TextArea(new Rect(65, 10, 100, 25), _toon.Name);
+	}
 
+	private void DisplayAttributes()
+	{
 		for (int count = 0; count < Enum.GetValues(typeof(AttributeName)).Length; count++)
 		{
 			GUI.Label(new Rect(10, 40 + (count * 25), 100, 25), ((AttributeName)count).ToString());
 			GUI.Label(new Rect(115, 40 + (count * 25), 30, 25), _toon.GetPrimaryAttribute(count).AdjustedBaseValue.ToString());
 		}
+	}
+
+	private void DisplayVitals()
+	{
+
+	}
+
+	private void DisplaySkills()
+	{
+		
 	}
 }
