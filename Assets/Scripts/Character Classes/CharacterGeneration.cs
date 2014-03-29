d168a58e78dca492bccbc2f8e68689361db3d07c
Nathan Kluth <nlkluth@gmail.com>
2014-03-29 17:23:03 -0600

    working on stats enum

--- Assets/Scripts/Character Classes/CharacterGeneration.cs
+++ Assets/Scripts/Character Classes/CharacterGeneration.cs
@@ -16,9 +16,9 @@ public class CharacterGeneration : MonoBehaviour
 		GUI.Label(new Rect(10, 10, 50, 25), "Name: ");
 		_toon.Name = GUI.TextArea(new Rect(65, 10, 100, 25), _toon.Name);
 
-//		for (int count = 0; count < Enum.GetValues(typeof(AttributeName)).Length; count++)
-//		{
-//
-//		}
+		for (int count = 0; count < Enum.GetValues(typeof(AttributeName)).Length; count++)
+		{
+
+		}
 	}
 }
