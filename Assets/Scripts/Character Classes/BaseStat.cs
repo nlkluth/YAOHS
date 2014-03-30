dd8d9deea536225a2ad22c1c1c31d4233481d86c
Nathan Kluth <nlkluth@gmail.com>
2014-03-30 16:29:04 -0600

    fix bad name value

---
+++ Assets/Scripts/Character Classes/BaseStat.cs
@@ -0,0 +1,32 @@
+﻿
+public class BaseStat
+{
+	public BaseStat()
+	{
+		BaseValue = 0;
+		BuffValue = 0;
+		LevelModifier = 1.1f;
+		ExpToLevel = 100;
+	}
+
+	public int BaseValue { get; set; }
+	public int BuffValue { get; set; }
+	public int ExpToLevel { get; set; }
+	public float LevelModifier { get; set; }
+
+	private int CalculateExpTolevel()
+	{
+		return (int)(ExpToLevel * LevelModifier);
+	}
+
+	public void LevelUp()
+	{
+		ExpToLevel = CalculateExpTolevel();
+		BaseValue++;
+	}
+
+	public int AdjustedBaseValue
+	{
+		get { return BaseValue + BuffValue; }
+	}
+}
