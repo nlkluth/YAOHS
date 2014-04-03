09fb2448255c79ce2e3ecfaf98b5091ce14be2c5
Nathan Kluth <nlkluth@gmail.com>
2014-04-03 17:31:08 -0600

    game settings

---
+++ Assets/Scripts/GameSettings.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameSettings : MonoBehaviour 
+{
+	public GameObject PlayerPrefab;	
+
+	void Awake()
+	{
+		DontDestroyOnLoad(this);
+	}
+	
+	public void SaveCharacterData()
+	{
+		PlayerPrefs.SetString("Player Name", "test");
+	}
+
+	public void LoadCharacterData()
+	{}
+}
