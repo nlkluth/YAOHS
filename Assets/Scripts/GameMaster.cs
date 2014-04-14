c14e1feddc088c119e356964737a585e364245ad
Nathan Kluth <nlkluth@gmail.com>
2014-04-13 19:01:51 -0600

    camera follows player

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -6,9 +6,13 @@ public class GameMaster : MonoBehaviour
 	public GameObject playerCharacter;
 	public Camera mainCamera;
 
+	private GameObject player;
+
 	void Start() 
 	{
-		Instantiate (playerCharacter, Vector3.zero, Quaternion.identity);
+		player = Instantiate (playerCharacter, Vector3.zero, Quaternion.identity) as GameMaster;
+
+		mainCamera.transform.position = player.transform.position;
 	}
 	
 	void Update()
