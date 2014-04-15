92ad935d31aa5ff50c43fa07874f38d04345c890
Nathan Kluth <nlkluth@gmail.com>
2014-04-14 22:25:30 -0600

    setting up camera position

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -5,14 +5,25 @@ public class GameMaster : MonoBehaviour
 {
 	public GameObject playerCharacter;
 	public Camera mainCamera;
+	public float zOffset;
+	public float yOffset;
+	public float xRotOffset;
 
-	private GameObject player;
+	private GameObject _player;
+	private PlayerCharacter _playerCharacterScript;
 
 	void Start() 
 	{
-		player = Instantiate (playerCharacter, Vector3.zero, Quaternion.identity) as GameMaster;
+		_player = Instantiate (playerCharacter, Vector3.zero, Quaternion.identity) as GameObject;
+		_playerCharacterScript = _player.GetComponent<PlayerCharacter>();
 
-		mainCamera.transform.position = player.transform.position;
+		zOffset = -2.5f;
+		yOffset = 2.5f;
+		xRotOffset = 22.5f;
+
+		mainCamera.transform.position = new Vector3(_player.transform.position.x, _player.transform.position.y + yOffset,
+		                                            _player.transform.position.z + zOffset);
+		mainCamera.transform.Rotate(xRotOffset, 0, 0);
 	}
 	
 	void Update()
