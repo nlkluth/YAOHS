37200e53b6196446fb962d04a8a6e45334a75af9
Nathan Kluth <nlkluth@gmail.com>
2014-04-24 14:34:07 -0600

    adjusting level

--- Assets/Scripts/HUD/VitalBar.cs
+++ Assets/Scripts/HUD/VitalBar.cs
@@ -3,15 +3,11 @@ using System.Collections;
 
 public class VitalBar : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	void Start () 
+	{}
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	void Update () 
+	{}
 
 	public void OnEnable()
 	{}
