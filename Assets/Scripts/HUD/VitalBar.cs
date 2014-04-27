7f7e42067f9a7df26988f68a1e41432c6b5d4d26
Nathan Kluth <nlkluth@gmail.com>
2014-04-27 14:42:28 -0600

    setting bar positions

--- Assets/Scripts/HUD/VitalBar.cs
+++ Assets/Scripts/HUD/VitalBar.cs
@@ -5,10 +5,16 @@ public class VitalBar : MonoBehaviour {
 	public bool _isPlayerHealthBar;
 	private int _maxBarLength;
 	private int _currentBarLength;
+	private GUITexture _display;
 
 	void Start () 
 	{
-		_isPlayerHealthBar = true;
+//		_isPlayerHealthBar = true;
+		_display = gameObject.GetComponent<GUITexture> ();
+
+		_maxBarLength = (int)_display.pixelInset.width;
+
+		OnEnable();
 	}
 	
 	void Update () 
@@ -34,7 +40,8 @@ public class VitalBar : MonoBehaviour {
 
 	public void OnHealthBarChanged(int maxHealth, int currentHealth)
 	{
-		_currentBarLength = (currentHealth / maxHealth) * _maxBarLength;
+		_currentBarLength = (int)((currentHealth / (float)maxHealth) * _maxBarLength);
+		_display.pixelInset = new Rect(_display.pixelInset.x, _display.pixelInset.y, _currentBarLength, _display.pixelInset.height);
 	}
 
 	public void SetPlayerHealthBar(bool flag)
