c2931d509788aa2bbc1097999fcb27a12bb5bd03
Nathan Kluth <nlkluth@gmail.com>
2014-05-14 17:54:03 -0600

    set starting value for mob

---
+++ Assets/Scripts/Mob.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mob : BaseCharacter 
+{
+	void Start ()
+	{
+		GetPrimaryAttribute(int)AttributeName.Constitution)).BaseValue = 100;
+	}
+
+	void Update () 
+	{
+		Messenger<int, int>.Broadcast("mob health update", 100, 80);
+	}
+}
