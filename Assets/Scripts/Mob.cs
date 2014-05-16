bc16dd3b8ad18d70fea0c9988c243c7432b8ab35
Nathan Kluth <nlkluth@gmail.com>
2014-05-15 18:33:30 -0600

    actual health values

--- Assets/Scripts/Mob.cs
+++ Assets/Scripts/Mob.cs
@@ -3,16 +3,23 @@ using System.Collections;
 
 public class Mob : BaseCharacter 
 {
+	public int currentHealth;
+	public int maxHealth;
+
 	void Start ()
 	{
-		GetPrimaryAttribute((int)AttributeName.Constitution).BaseValue = 100;
-		GetVital((int)VitalName.Health).Update();
+//		GetPrimaryAttribute((int)AttributeName.Constitution).BaseValue = 100;
+//		GetVital((int)VitalName.Health).Update();
 
 		Name = "Spidey";
 	}
 
 	void Update () 
 	{
-		//Messenger<int, int>.Broadcast("mob health update", 100, 80);
+	}
+
+	public void DisplayHealth()
+	{
+		Messenger<int, int>.Broadcast("mob health update", currentHealth, maxHealth);
 	}
 }
