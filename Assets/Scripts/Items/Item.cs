1f75c64f6ec0f6832c8b70bebedd5a3a3e2bd0f0
Nathan Kluth <nlkluth@gmail.com>
2014-05-21 16:25:16 -0600

    adding base item class and enum

---
+++ Assets/Scripts/Items/Item.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item
+{
+	public string Name { get; set; }
+	public int Value { get; set; }
+	public Rarity Rarity { get; set; }
+}
+
+public enum Rarity 
+{
+	Common,
+	Uncommon,
+	Rare
+}
