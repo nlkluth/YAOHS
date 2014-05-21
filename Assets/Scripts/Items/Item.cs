cf15876ee9ed7e0421ad30f08c7bba756887fbfd
Nathan Kluth <nlkluth@gmail.com>
2014-05-21 16:28:08 -0600

    durability and default constructor

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -6,6 +6,17 @@ public class Item
 	public string Name { get; set; }
 	public int Value { get; set; }
 	public Rarity Rarity { get; set; }
+	public int CurrentDurability { get; set; }
+	public int MaxDurability { get; set; }
+
+	public Item()
+	{
+		Name = "Need Name";
+		Value = 0;
+		Rarity = Rarity.Common;
+		MaxDurability = 50;
+		CurrentDurability = MaxDurability;
+	}
 }
 
 public enum Rarity 
