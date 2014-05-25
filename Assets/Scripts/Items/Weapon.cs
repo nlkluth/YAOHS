3aff55b81f905c95453b6f5444379ec56b28aa8d
Nathan Kluth <nlkluth@gmail.com>
2014-05-24 23:04:49 -0600

    setting up default & overloaded constructors

--- Assets/Scripts/Items/Weapon.cs
+++ Assets/Scripts/Items/Weapon.cs
@@ -1,7 +1,34 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class Weapon : BuffItem 
+﻿public class Weapon : BuffItem 
 {
+	private int _maxDamage;
+	private float _damageVariable;
+	private float _maxRange;
+	private DamageType _damageType;
 
+	public Weapon()
+	{
+		_maxDamage = 0;
+		_damageVariable = 0f;
+		_maxRange = 0f;
+		_damageType = DamageType.Bludgeon;
+	}
+
+	public Weapon(int maxDamage, float damageVariable, float maxRange, DamageType damageType)
+	{
+		_maxDamage = maxDamage;
+		_damageVariable = damageVariable;
+		_maxRange = maxRange;
+		_damageType = damageType;
+	}
+}
+
+public enum DamageType
+{
+	Bludgeon,
+	Pierce,
+	Slash,
+	Fire,
+	Ice,
+	Lightning,
+	Acid
 }
