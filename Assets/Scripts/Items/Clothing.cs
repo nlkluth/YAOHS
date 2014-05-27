6e210317f1b9572443b9c147a7cf46b5cef040e4
Nathan Kluth <nlkluth@gmail.com>
2014-05-27 15:44:54 -0600

    constructors

--- Assets/Scripts/Items/Clothing.cs
+++ Assets/Scripts/Items/Clothing.cs
@@ -3,7 +3,17 @@ using System.Collections;
 
 public class Clothing : BuffItem 
 {
+	public ArmorSlot Slot { get; set; }
 
+	public Clothing()
+	{
+		Slot = ArmorSlot.Head;
+	}
+
+	public Clothing(ArmorSlot slot)
+	{
+		Slot = slot;
+	}
 }
 
 public enum ArmorSlot
