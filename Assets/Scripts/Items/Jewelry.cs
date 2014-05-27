2c81f1f44b809e5e6e9ac2004514e6e0ed96f3af
Nathan Kluth <nlkluth@gmail.com>
2014-05-27 14:02:33 -0600

    getter/setter and constructor

--- Assets/Scripts/Items/Jewelry.cs
+++ Assets/Scripts/Items/Jewelry.cs
@@ -2,7 +2,17 @@
 
 public class Jewelry : BuffItem
 {
-	
+	public JewelrySlot Slot { get; set; }
+
+	public Jewelry() 
+	{
+		Slot = JewelrySlot.Necklace;
+	}
+
+	public Jewelry(JewelrySlot slot)
+	{
+		Slot = slot;
+	}
 }
 
 public enum JewelrySlot
