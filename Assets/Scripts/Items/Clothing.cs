09a9745a8faa3836793d8d0867b2c68c0ca98d20
Nathan Kluth <nlkluth@gmail.com>
2014-05-27 15:42:37 -0600

    setting up armor

---
+++ Assets/Scripts/Items/Clothing.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Clothing : BuffItem 
+{
+
+}
+
+public enum ArmorSlot
+{
+	Head,
+	Shoulder,
+	Chest,
+	Back,
+	Belt,
+	Legs,
+	Feet,
+	Hands
+}
