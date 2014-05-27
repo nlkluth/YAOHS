d43771d40bd216dbd80ea3623fec37cdcc3ef2be
Nathan Kluth <nlkluth@gmail.com>
2014-05-27 15:58:20 -0600

    setting up buffitem

--- Assets/Scripts/Items/Consumable.cs
+++ Assets/Scripts/Items/Consumable.cs
@@ -1,7 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Consumable : MonoBehaviour 
+public class Consumable : BuffItem
 {
-	
+	public Vital[] Vital;
+	public int[] AmountToHeal;
+	public float BuffTime;
+
+	public Consumable() 
+	{
+		Reset();
+	}
+
+	public Consumable(Vital[] vital, int[] amountToHeal, float buffTime)
+	{
+		Vital = vital;
+		AmountToHeal = amountToHeal;
+		BuffTime = buffTime;
+	}
+
+	public void Reset()
+	{
+		BuffTime = 0;
+
+		for (int count = 0; count < Vital.Length; count++)
+		{
+			Vital[count] = new Vital();
+			AmountToHeal[count] = 0;
+		}
+	}
+
+	public int VitalCount()
+	{
+		return Vital.Length;
+	}
 }
