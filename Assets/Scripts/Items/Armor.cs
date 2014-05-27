b7f2a9d2b40d1beb712e5d191d3e3c5748c66678
Nathan Kluth <nlkluth@gmail.com>
2014-05-27 15:47:04 -0600

    armor class

---
+++ Assets/Scripts/Items/Armor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Armor : Clothing 
+{
+	public int ArmorLevel { get; set; }
+
+	public Armor()
+	{
+		ArmorLevel = 0;
+	}
+
+	public Armor(int armorLevel)
+	{
+		ArmorLevel = armorLevel;
+	}
+}
