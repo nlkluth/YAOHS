7097e09d5a79aa1c3006a5b0a34b35128241862b
Nathan Kluth <nlkluth@gmail.com>
2014-05-30 12:59:55 -0600

    adding gametime script

---
+++ Assets/Scripts/GameTime.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameTime : MonoBehaviour 
+{
+	public Transform[] sun;
+	public float dayCycleInMinutes = 1;
+
+	private const float SECOND = 1;
+	private const float MINUTE = 60 * SECOND;
+	private const float HOUR = 60 * MINUTE;
+	private const float DAY = 24 * HOUR;
+	private const float DEGREES_PER_SECOND = 360 / DAY;
+
+	private float _degreeRotation;
+	private float _timeOfDay;
+
+	void Start()
+	{
+		_timeOfDay = 0;
+		_degreeRotation = DEGREES_PER_SECOND * DAY / (dayCycleInMinutes * MINUTE);
+	}
+	
+	void Update () 
+	{
+		sun[0].Rotate(new Vector3(_degreeRotation, 0, 0) * Time.deltaTime);
+		_timeOfDay += Time.deltaTime;
+	}
+}
