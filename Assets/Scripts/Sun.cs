0687c8c6ac8ec9c3fd4df28db874791d7aa790a1
Nathan Kluth <nlkluth@gmail.com>
2014-05-31 23:06:38 -0600

    setting up the sun script

---
+++ Assets/Scripts/Sun.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("Environments/Sun")]
+public class Sun : MonoBehaviour
+{
+	public float maxBrightness;
+	public float minBrightness;
+}
